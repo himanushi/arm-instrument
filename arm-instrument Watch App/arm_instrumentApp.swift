ecffa1214b6e63e1af3d1c503bf422360449f627
himanushi <himanushi@users.noreply.github.com>
2022-12-21 05:35:01 +0900

    Initial Commit

---
+++ arm-instrument Watch App/arm_instrumentApp.swift
@@ -0,0 +1,17 @@
+//
+//  arm_instrumentApp.swift
+//  arm-instrument Watch App
+//
+//  Created by 匿名 on 2022/12/21.
+//
+
+import SwiftUI
+
+@main
+struct arm_instrument_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ arm-instrument/arm_instrumentApp.swift
@@ -0,0 +1,17 @@
+//
+//  arm_instrumentApp.swift
+//  arm-instrument
+//
+//  Created by 匿名 on 2022/12/21.
+//
+
+import SwiftUI
+
+@main
+struct arm_instrumentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
