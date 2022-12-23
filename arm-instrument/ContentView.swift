dfa4505a8c2cada0d531f37824fbe217e050dabc
himanushi <himanushi@users.noreply.github.com>
2022-12-23 09:01:50 +0900

    音調整

--- arm-instrument/ContentView.swift
+++ arm-instrument/ContentView.swift
@@ -11,21 +11,19 @@ import CoreMotion
 struct ContentView: View {
     private let motionManager = CMMotionManager()
     private let tone = ToneOutputUnit()
-    
-    @State private var x: Double = 0.0
     @State private var y: Double = 0.0
-    @State private var z: Double = 0.0
+    @State private var hzData: Double = 0.0
     
     var body: some View {
         VStack(spacing: 50) {
-            Text("y: \((y + 2) * 1760 )")
+            Text("Hz: \(hz(y))")
+            Text("A4 = 440 Hz")
             Button {
                 tone.enableSpeaker()
                 tone.setToneTime(t: 10000)
             } label: {
                 Text("押して")
             }
-
         }
         .onAppear() {
             start()
@@ -36,15 +34,18 @@ struct ContentView: View {
         }
     }
     
+    func hz(_ slope: Double) -> Double {
+        hzData = (slope + 1) * 1760
+        return hzData
+    }
+    
     func start() {
         if motionManager.isAccelerometerAvailable {
             motionManager.accelerometerUpdateInterval = 0.1
             motionManager.startAccelerometerUpdates(to: .main) { data, error in
                 guard let data else { return }
-                tone.setFrequency(freq: (data.acceleration.y > 0 ? data.acceleration.y : data.acceleration.y + 1) * 1760 )
-                x = data.acceleration.x
                 y = data.acceleration.y
-                z = data.acceleration.z
+                tone.setFrequency(freq: hz(y))
             }
         }
     }
